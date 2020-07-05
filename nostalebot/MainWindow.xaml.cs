b103d72d8a5dd8b7a66d46f30b9a2909c069d8a6
Minemug <34980966+Minemug@users.noreply.github.com>
2020-07-05 20:45:01 +0200

    Upload files

---
+++ nostalebot/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace nostalebot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        [DllImportAttribute("User32.dll")]
+        private static extern int FindWindow(String ClassName, String WindowName);
+        [DllImportAttribute("User32.dll")]
+        private static extern IntPtr SetForegroundWindow(int hWnd);
+        public MainWindow()
+        {
+            InitializeComponent();         
+        }
+
+        Methods Checkers = new Methods();
+
+        bool work = true;
+
+        private void ClickScanButton(object sender, RoutedEventArgs e)
+        {
+            var Intervaltime = InputBox.Text;
+            bool success = Int32.TryParse(Intervaltime, out int time);
+            if (success == false)
+            {
+                MessageBox.Show("Nie wprowadziles interwału");
+                work = false;
+            } 
+                               
+            while (work == true)
+            {                
+                Bitmap screenshot = Screenshot();
+                Screenshot();
+                //screenshot.Save("C:/tmp/test.jpeg", ImageFormat.Jpeg);
+                Checkers.CheckPercantage(screenshot);
+                Checkers.CheckIfRaid(screenshot);
+                Thread.Sleep(time * 60 * 1000);
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// takes the screenshot
+        /// </summary>
+        /// <returns></returns>
+        private Bitmap Screenshot()
+        {
+            int hWnd = FindWindow(null, "NosTale");
+            //System.Windows.MessageBox.Show($"{ hWnd}");
+            if (hWnd == 0)
+            {
+                MessageBox.Show("Nie znalazłem NosTale");
+                work = false;
+                
+            }
+            SetForegroundWindow(hWnd);
+            Thread.Sleep(500);            
+            Bitmap bmpScreeenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            var g = Graphics.FromImage(bmpScreeenshot);
+
+            g.CopyFromScreen(877, 967, 877, 967, Screen.PrimaryScreen.Bounds.Size);
+
+           
+            return bmpScreeenshot;
+
+        }
+    }
+}
---
+++ nostalebot/Methods.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace nostalebot
+{
+    class Methods
+    {       
+
+        /// <summary>
+        /// Checking if raid is now
+        /// </summary>
+        /// <param name="screenshot"></param>
+        public void CheckIfRaid(Bitmap screenshot)
+        {
+            Color RaidCheckerDemons = screenshot.GetPixel(1029, 976);
+            if (RaidCheckerDemons.R == 229)
+            {
+                               
+                SendKeys.SendWait($"Mamy rajdzik !");
+                Thread.Sleep(500);
+                SendKeys.SendWait("{ENTER}");
+            }
+            Color RaidCheckerAngels = screenshot.GetPixel(889, 978);
+            if (RaidCheckerAngels.R == 255)
+            {
+                
+                SendKeys.SendWait($"Anioły mają rajd");
+                Thread.Sleep(500);
+                SendKeys.SendWait("{ENTER}");               
+            }
+        }
+
+        int leftsideofbar = 1030;
+        int rightsideofbar = 1341;
+        int Rcolorofbar = 185;
+
+        /// <summary>
+        /// Checking percentage of raid
+        /// </summary>
+        /// <param name="screenshot"></param>
+        public void CheckPercantage(Bitmap screenshot)
+        {
+            
+            for (int i = leftsideofbar; i < rightsideofbar; i+=2)
+            {
+                Color Checker = screenshot.GetPixel((int)i, 976);
+
+                if (Checker.R == Rcolorofbar || Checker.R == Rcolorofbar-1)
+                {
+                    float Finalvalue = CalculatePerc(i);
+
+                    Color MoreThan90 = screenshot.GetPixel((int)1049, 978);
+                    if (MoreThan90.R == 154)
+                    {
+                        SendKeys.SendWait($"Za chwile bedzie rajd! (wiecej niz 90 procent)");
+                        Thread.Sleep(500);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    else
+                    {
+                        SendKeys.SendWait($"Na glacy jest {Finalvalue} procent.");
+                        Thread.Sleep(500);
+                        SendKeys.SendWait("{ENTER}");
+                    }
+                    break;
+                }
+            }
+
+            
+        }
+
+        private static float CalculatePerc(int i)
+        {
+            float percantage = ((float)(i - 1030) / (float)311) * 100;
+            percantage = (float)System.Math.Round(percantage, 0);
+            var Finalvalue = 101 - percantage;
+            return Finalvalue;
+        }
+    }
+}
